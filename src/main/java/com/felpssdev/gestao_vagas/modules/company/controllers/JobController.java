5d1f69af9a250db8225b2934bba5b345b16a4196
felpssm <felpss.martinssf@gmail.com>
2025-04-30 14:42:32 -0300

    feat: create a job controller

--- src/main/java/com/felpssdev/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/felpssdev/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,10 +1,13 @@
 package com.felpssdev.gestao_vagas.modules.company.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felpssdev.gestao_vagas.modules.company.entities.JobEntity;
+import com.felpssdev.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 
 import jakarta.validation.Valid;
 
@@ -12,7 +15,11 @@ import jakarta.validation.Valid;
 @RequestMapping("job")
 public class JobController {
 
-    public void create(@Valid @RequestBody JobEntity jobEntity) {
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
 
+    @PostMapping("/")
+    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+        return this.createJobUseCase.execute(jobEntity);
     }
 }
