9a9309f7e68cc7640df73c0c2fa884c0072f5955
felpssm <felpss.martinssf@gmail.com>
2025-04-30 16:23:23 -0300

    feat: ajust securityconfig

--- src/main/java/com/felpssdev/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/felpssdev/gestao_vagas/security/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @SuppressWarnings("unused")
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csfr -> csfr.disable());
