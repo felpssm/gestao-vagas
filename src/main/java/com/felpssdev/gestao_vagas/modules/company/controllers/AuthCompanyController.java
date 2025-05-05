d899e2d4c47cc35ca2d2f1e5b2746e8fddf0079a
felpssm <felpss.martinssf@gmail.com>
2025-05-05 15:28:21 -0300

    feat: configured securityfilter

--- src/main/java/com/felpssdev/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/com/felpssdev/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -3,6 +3,8 @@ package com.felpssdev.gestao_vagas.modules.company.controllers;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,12 @@ public class AuthCompanyController {
     private AuthCompanyUseCase authCompanyUseCase;
 
     @PostMapping("/company")
-    public String create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
-        return this.authCompanyUseCase.execute(authCompanyDTO);
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+        try {
+        var result = this.authCompanyUseCase.execute(authCompanyDTO);
+        return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/felpssdev/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/felpssdev/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package com.felpssdev.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +42,7 @@ public class AuthCompanyUseCase {
         }
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         var token = JWT.create().withIssuer("javacompany")
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getId().toString())
                 .sign(algorithm);
         return token;
